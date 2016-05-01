a668eb60c055b5c5b3dfb3114ab75bdef2b1b43b
Dzmitry Spirydzionak <dzmitry.spirydzionak@tomra.com>
2016-05-01 12:14:45 +0200

    task 1 data binding added

--- WK2/conFusion/app/scripts/app.js
+++ WK2/conFusion/app/scripts/app.js
@@ -163,14 +163,21 @@ angular.module('confusionApp', [])
             
             //Step 1: Create a JavaScript object to hold the comment from the form
             
+            $scope.newComment = {
+                name: '',
+                numOfStars: '5',
+                comments: ''
+            };
+            //$scope.numOfStars = '5';
+
             $scope.submitComment = function () {
                 
                 //Step 2: This is how you record the date
                 //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
                 
                 // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push("Your JavaScript Object holding the comment");
-                
+                //$scope.dish.comments.push("Your JavaScript Object holding the comment");
+                console.log($scope.newComment);
                 //Step 4: reset your form to pristine
                 
                 //Step 5: reset your JavaScript object that holds your comment
